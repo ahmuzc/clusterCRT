f3c85de0bf056b984c5d9b0aad6923ff77d7ff88
ahmuzc <89759644+ahmuzc@users.noreply.github.com>
2023-02-15 10:39:09 +0800

    Update eventrate_samplesize.R

--- eventrate_samplesize.R
+++ eventrate_samplesize.R
@@ -17,3 +17,9 @@ cluster.eventrate <- function(a=0.05,b=0.2,lambda0,lambda1,cv,y){
   print(paste0('total pyears per arm ',py))
   print(paste0('estimated design effect ',de))
 }
+
+# Accrombessi M, Cook J, Dangbenon E, et al. Efficacy of pyriproxyfen-pyrethroid long-lasting insecticidal nets (LLINs) and chlorfenapyr-pyrethroid LLINs compared with pyrethroid-only LLINs for malaria control in Benin: a cluster-randomised, superiority trial. Lancet. 2023;401(10375):435-446. doi:10.1016/S0140-6736(22)02319-4
+# cluster.eventrate(a=0.025,lambda0 = 1,lambda1 =0.7,cv=0.3,y=20*2)
+# "required No. of clusters per arm 20"
+# "total pyears per arm 800"
+# "estimated design effect 4.46"
